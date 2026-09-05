56b87bc123d3acf416092dde4b7eaaa03149bf72
Gustavo Uselis <uselis.ads.facens@gmail.com>
2026-09-04 22:15:35 -0300

    Primeiro Commit do Projeto

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //main do Nosso Projeto -
+        // usar branches para
+        // fazer alterações e pull requests para merge na main.
+    }
+}
